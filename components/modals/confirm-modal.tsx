82c2ff743f2542d185336a9bc134bbbda14c72df
Ankur123UnsungDevloper <110739503+Ankur123unsungDevloper@users.noreply.github.com>
2024-02-29 17:30:29 +0530

    modified:   components/modals/confirm-modal.tsx

--- components/modals/confirm-modal.tsx
+++ components/modals/confirm-modal.tsx
@@ -1,1 +1,24 @@
 "use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle
+} from "@/components/ui/alert-dialog";
+
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+};
+
+export const ConfirmModal = ({
+  children,
+  onConfirm
+}: ConfirmModalProps) => {
+  
+}
