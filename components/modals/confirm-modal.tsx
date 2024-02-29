45d1d303170f61353d090fca1e6fb108175c70bf
Ankur123UnsungDevloper <110739503+Ankur123unsungDevloper@users.noreply.github.com>
2024-02-29 18:14:45 +0530

    modified:   app/(main)/_components/trash-box.tsx 	modified:   components/modals/confirm-modal.tsx 	new file:   components/search-command.tsx 	modified:   components/ui/alert-dialog.tsx 	new file:   components/ui/command.tsx 	modified:   components/ui/skeleton.tsx 	new file:   hooks/use-search.tsx 	modified:   package-lock.json 	modified:   package.json

--- components/modals/confirm-modal.tsx
+++ components/modals/confirm-modal.tsx
@@ -8,7 +8,8 @@ import {
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
-  AlertDialogTitle
+  AlertDialogTitle,
+  AlertDialogTrigger
 } from "@/components/ui/alert-dialog";
 
 interface ConfirmModalProps {
@@ -20,5 +21,39 @@ export const ConfirmModal = ({
   children,
   onConfirm
 }: ConfirmModalProps) => {
-  
+  const handleConfirm = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    onConfirm();
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger
+        onClick={(e) => e.stopPropagation()}
+        asChild
+      >
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are  you absolutely sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action can&apos;t be undone!
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={e => e.stopPropagation()}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={handleConfirm}>
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
 }
