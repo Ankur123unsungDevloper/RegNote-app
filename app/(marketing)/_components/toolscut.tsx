4f2d0587b6f92a6971f6ca974d879815d0ade9e7
Ankur123UnsungDevloper <110739503+Ankur123unsungDevloper@users.noreply.github.com>
2024-03-14 21:28:18 +0530

    new file:   app/(marketing)/_components/card.tsx 	modified:   app/(marketing)/_components/page.css 	new file:   app/(marketing)/_components/toolscut.tsx 	modified:   app/(marketing)/page.tsx 	new file:   public/pencile.svg

---
+++ app/(marketing)/_components/toolscut.tsx
@@ -0,0 +1,9 @@
+export const Toolscut = () => {
+  return (
+    <div className="toolcut grid">
+      <div className="tool grid-item span-12 no-row-gap">
+        Toolscut
+      </div>
+    </div>
+  )
+}
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,13 +1,17 @@
+import { Card } from "./_components/card";
 import { Footer } from "./_components/footer";
 import { Heading } from "./_components/heading";
 import { Heroes } from "./_components/heroes";
+import { Toolscut } from "./_components/toolscut";
 
 const MarketingPage = () => {
   return ( 
-    <div className="min-h-full flex flex-col">
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heroes />
+        <Card />
+        <Toolscut />
       </div>
       <Footer />
     </div>
